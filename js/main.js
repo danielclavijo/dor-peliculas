45f5d6fa9fc251efd38018d77d39fc16d8e61fe6
danielclavijo <daniel_lc_97@hotmail.com>
2018-03-09 09:09:26 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ function loadMovies(){
 
                 votingInfo.push([apiData.results[i].title]);
 
-                var movieDiv = $("<div></div>",{'class': 'movie','id':'movie'+i, 'aria-label':apiData.results[i].title,'tabindex':i}).appendTo("#movieWrapper")
+                var movieDiv = $("<div></div>",{'class': 'movie','id':'movie'+i, 'aria-label':apiData.results[i].title,'tabindex':i+1}).appendTo("#movieWrapper")
                     .append($('<img>',{'src':  apiData.results[i].thumbnail.path + "." + apiData.results[i].thumbnail.extension}));
 
                     $("<div class='info-div' id='info" + i +"'><b>Título:</b> " + apiData.results[i].title +
